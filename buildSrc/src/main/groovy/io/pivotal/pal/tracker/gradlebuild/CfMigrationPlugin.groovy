7258c6f06403e9bf0ad9579ba8e4e20f7dcc6729
Neil Winton <nwinton@pivotal.io>
2019-02-15 16:16:27 +0530

    Handled getting credentials for databases using CredHub. Added test for tunnel connectivity instead of hard-coded wait.

--- buildSrc/src/main/groovy/io/pivotal/pal/tracker/gradlebuild/CfMigrationPlugin.groovy
+++ buildSrc/src/main/groovy/io/pivotal/pal/tracker/gradlebuild/CfMigrationPlugin.groovy
@@ -8,10 +8,12 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class CfMigrationPlugin implements Plugin<Project> {
+    private final static int TUNNEL_PORT = 63306
 
     @Override
     void apply(Project project) {
         Process tunnelProcess = null
+        Map credentials = null
 
         project.with {
             afterEvaluate {
@@ -19,13 +21,22 @@ class CfMigrationPlugin implements Plugin<Project> {
                 def appName = databases.cfApp
                 def databaseInstanceName = databases.cfDatabase
 
+                task( "acquireCredentials") {
+                    doLast {
+                        println "Acquiring database credentials"
+                        credentials = acquireMysqlCredentials(appName, databaseInstanceName)
+                    }
+                }
+
                 task("openTunnel") {
+                    dependsOn "acquireCredentials"
                     doLast {
                         println "Opening Tunnel for $appName"
                         Thread.start {
-                            tunnelProcess = "cf ssh -N -L 63306:${getMysqlHost(appName, databaseInstanceName)}:3306 $appName".execute()
+                            tunnelProcess = "cf ssh -N -L ${TUNNEL_PORT}:${credentials["hostname"]}:${credentials["port"]} $appName".execute()
                         }
-                        sleep 5_000L
+
+                        waitForTunnelConnectivity()
                     }
                 }
 
@@ -39,41 +50,52 @@ class CfMigrationPlugin implements Plugin<Project> {
                 task("cfMigrate", type: FlywayMigrateTask, group: "Migration") {
                     dependsOn "openTunnel"
                     finalizedBy "closeTunnel"
-                    doFirst { extension = buildFlywayExtension(project, appName, databaseInstanceName) }
+                    doFirst { extension = buildFlywayExtension(project, appName, databaseInstanceName, credentials) }
                 }
 
                 task("cfRepair", type: FlywayRepairTask, group: "Migration") {
                     dependsOn "openTunnel"
                     finalizedBy "closeTunnel"
-                    doFirst { extension = buildFlywayExtension(project, appName, databaseInstanceName) }
+                    doFirst { extension = buildFlywayExtension(project, appName, databaseInstanceName, credentials) }
                 }
             }
         }
     }
 
-    private def getMysqlHost(cfAppName, databaseInstanceName) {
-        return getMysqlCredentials(cfAppName, databaseInstanceName)["hostname"]
+    private static void waitForTunnelConnectivity() {
+        int remainingAttempts = 20
+        while (remainingAttempts > 0) {
+            remainingAttempts--
+            try {
+                new Socket('localhost', TUNNEL_PORT).close()
+                remainingAttempts = 0
+            } catch (ConnectException e) {
+                println "Waiting for tunnel ($remainingAttempts attempts remaining)"
+                sleep 1_000L
+            }
+        }
     }
 
-    private static def buildFlywayExtension(Project project, String cfAppName, databaseInstanceName) {
+    private static def buildFlywayExtension(Project project, String cfAppName, databaseInstanceName, credentials) {
         def extension = new FlywayExtension()
 
-        getMysqlCredentials(cfAppName, databaseInstanceName)?.with { credentials ->
-
-            extension.user = credentials["username"]
-            extension.password = credentials["password"]
-            extension.url = "jdbc:mysql://127.0.0.1:63306/${credentials["name"]}"
-        }
+        extension.user = credentials["username"]
+        extension.password = credentials["password"]
+        extension.url = "jdbc:mysql://127.0.0.1:${TUNNEL_PORT}/${credentials["name"]}"
 
         extension.locations = ["filesystem:$project.projectDir/migrations"]
         return extension
     }
 
-    private static def getMysqlCredentials(cfAppName, databaseInstanceName) {
-        def appGuid = execute("cf app $cfAppName --guid").trim()
-        def envResponse = execute("cf curl /v2/apps/$appGuid/env")
-        def envJson = new JsonSlurper().parseText(envResponse)
-        def vcapServicesMap = envJson["system_env_json"]?.getAt("VCAP_SERVICES")
+    // The shell/perl 'magic' here is needed because where a database
+    // service uses CredHub, the credentials are not (for obvious reasons)
+    // available via 'cf env' or direct calls to the API. However, the
+    // expanded versions *are* presented to the running Java application
+    // in its environment, which we can access via the /proc filesystem.
+    private static Map acquireMysqlCredentials(cfAppName, databaseInstanceName) {
+        def vcapServicesJson = execute(['cf', 'ssh', cfAppName, '-c',
+                                        'perl -0 -ne "print if (s/^VCAP_SERVICES=//)" /proc/$(pgrep java)/environ'])
+        def vcapServicesMap = new JsonSlurper().parseText(vcapServicesJson)
 
         def entryWithDbInstance = vcapServicesMap
                 .find { key, value -> value.any { it["name"] == databaseInstanceName } }
@@ -84,8 +106,8 @@ class CfMigrationPlugin implements Plugin<Project> {
         return dbInstance["credentials"]
     }
 
-    private static String execute(String command) {
-        def process = command.execute()
+    private static String execute(List args) {
+        def process = args.execute()
         def output = process.text
         process.waitFor()
         return output
